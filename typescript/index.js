21eec0a2d85b97b22724339558923a130b3d4f72
MartinNuc <martin.nuc@productboard.com>
2023-04-04 13:17:07 +0200

    calculator

--- typescript/index.js
+++ typescript/index.js
@@ -1,2 +1,13 @@
 "use strict";
 console.log('Hello world');
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function sumOfArray(array) {
+    return array.reduce((acc, curr) => acc + curr, 0);
+}
+console.log(add(5, 3));
+console.log(sumOfArray([1, 2, 3, 4]));
--- typescript/index.ts
+++ typescript/index.ts
@@ -1,1 +1,16 @@
 console.log('Hello world');
+
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+function subtract(a: number, b: number): number {
+  return a - b;
+}
+
+function sumOfArray(array: Array<number>): number {
+  return array.reduce((acc, curr) => acc + curr, 0);
+}
+
+console.log(add(5, 3));
+console.log(sumOfArray([1,2,3,4]));
